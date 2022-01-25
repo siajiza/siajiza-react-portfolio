3291642f12e041613c25388cb114b2aec3563cc4
siajiza <siajiza.siajiza@gmail.com>
2022-01-25 12:04:57 +0100

    Initial commit

---
+++ src/components/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Siajiza</h1>
+        <h1>Silvia Jimenez Zapata</h1>
+        <h1>DevCamp React Starter</h1>
+        <h2>React Redux Router</h2>
+      </div>
+    );
+  }
+}
